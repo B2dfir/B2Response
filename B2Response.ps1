9fec73531cd834e64cff154a401e922c072bf896
B2dfir <barnabyskeggs@gmail.com>
2018-04-12 10:22:56 -0700

    Update B2Response.ps1

--- B2Response.ps1
+++ B2Response.ps1
@@ -8,7 +8,7 @@
 .OUTPUTS
   Logs are stored in a date/time stamped folder within the executed directory
 .NOTES
-  Version:        0.2
+  Version:        0.1
   Author:         Barnaby Skeggs
   Creation Date:  28 Jan 2018
   Purpose/Change: Initial script development
@@ -46,21 +46,39 @@ catch{
     $_.Exception.Message
 }
 
-#Create temporary folder on remote host for binary and file transfer
-try{
-    If (-NOT(Invoke-Command -Session $s1 -ScriptBlock {Test-Path \\$computerName\C$\Windows\Temp\B2R})){
-        Write-Host "Creating temporary directory \\$computerName\C$\Windows\Temp\B2R"
-        New-Item -Path "\\$computerName\C$\Windows\Temp\B2R" -ItemType directory >$null 2>&1
-    }
-    Else{
-        Write-Host "Temporary directory already exists: \\$computerName\C$\Windows\Temp\B2R"
-    }
+Function TempDir($tempDir){
+	#Create temporary folder on remote host for binary and file transfer
+	try{
+		If (-NOT(Invoke-Command -Session $s1 -ScriptBlock {param($tempDir) Test-Path C:\Windows\Temp\$tempDir} -ArgumentList ($tempDir))){
+			Write-Host "Creating temporary directory \\$computerName\C$\Windows\Temp\$tempDir"
+			New-Item -Path "\\$computerName\C$\Windows\Temp\$tempDir" -ItemType directory >$null 2>&1
+		}
+		Else{
+			Write-Host "Temporary directory already exists: \\$computerName\C$\Windows\Temp\$tempDir"
+		}
+	}
+	catch{
+		Write-Host "Error creating temporary directory on remote host"
+		$_.Exception.Message
+	}
 }
-catch{
-    Write-Host "Error creating temporary directory on remote host"
-    $_.Exception.Message
+Function LogSubDir($directoryName){
+	#Create temporary folder on remote host for binary and file transfer
+	try{
+		If (-NOT(Invoke-Command -Session $s1 -ScriptBlock {Test-Path \\$computerName\C$\Windows\Temp\B2R\$directoryName})){
+			Write-Host "Creating logging subdirectory $Logs\$directoryName"
+			New-Item -Path "$Logs\$directoryName" -ItemType directory >$null 2>&1
+		}
+		Else{
+			Write-Host "Logging subdirectory already exists: $Logs\$directoryName"
+		}
+	}
+	catch{
+		Write-Host "Error creating temporary directory on remote host"
+		$_.Exception.Message
+	}
 }
-
+	
 #Create Logging Directory
 New-Item -Path $Logs -ItemType directory >$null 2>&1
 Write-Host "Logging to " ((Resolve-Path .\).Path).trim()\$Logs\
@@ -68,7 +86,7 @@ Write-Host "Logging to " ((Resolve-Path .\).Path).trim()\$Logs\
 #Functinon to read and validate user input
 Function Menu{
     $Command = Read-Host "[$computerName]> "
-    if ($Command -ne 'dir' -and $Command -ne 'ls' -and $Command -ne 'help' -and $Command -ne '-h' -and $Command -ne 'pslist' -and $Command -ne 'cleanup' -and $Command -ne 'autorunsc' -and $Command -ne "browsinghistoryview" -and $Command -ne "bhv" -and $Command -ne "lav" -and $Command -ne "lastactivityview" -and $Command -ne 'prefetch' -and (-Not($Command.StartsWith('sigcheck'))) -and $Command -ne 'rekal' -and $Command -ne "users" -and $Command -ne "netstat" -and $Command -ne "dnscache" -and $Command -ne 'exit' -and (-Not($Command.StartsWith('cd'))) -and (-Not($Command.StartsWith('mkdir'))) -and (-Not($Command.StartsWith('exec'))) -and (-Not($Command.StartsWith('upload'))) -and (-Not($Command.StartsWith('download'))) -and $Command -ne 'quit'){
+    if ($Command -ne 'dir' -and $Command -ne 'ls' -and $Command -ne 'help' -and $Command -ne '-h' -and $Command -ne 'pslist' -and (-Not($Command.StartsWith('rawcopy'))) -and $Command -ne 'ps' -and $Command -ne 'cleanup' -and $Command -ne 'autorunsc' -and $Command -ne "browsinghistoryview" -and $Command -ne "bhv" -and $Command -ne "lav" -and $Command -ne "lastactivityview" -and $Command -ne 'prefetch' -and (-Not($Command.StartsWith('sigcheck'))) -and $Command -ne 'rekal' -and $Command -ne "users" -and $Command -ne "netstat" -and $Command -ne "dnscache" -and $Command -ne 'exit' -and (-Not($Command.StartsWith('cd'))) -and (-Not($Command.StartsWith('mkdir'))) -and (-Not($Command.StartsWith('exec'))) -and (-Not($Command.StartsWith('upload'))) -and (-Not($Command.StartsWith('download'))) -and $Command -ne 'quit'){
         Write-Host "Unknown command. Type 'help' or '-h' for command list"
         }
     return $Command
@@ -77,6 +95,8 @@ Function Menu{
 #Help Menu
 Function Help{
     $help = @"
+	
+	#Need to add timestamps to logs
 
     Available commands:
     
@@ -87,12 +107,22 @@ Function Help{
     cd                           Change current directory
     mkdir                        Create directory
     pslist              ps       List running processes
-    upload                       Uploads a file to remote host. Must use absolute paths and must wrap in double quotes
+    upload                       Uploads a file from uploads folder to remote host.
                                  Syntax: 
-                                 upload "C:\local\host\test.txt" "C:\remote\host\test.txt"
-    download                     Download a file from remote host. Must use absolute paths and must wrap in double quotes
-                                 Syntax: 
-                                 download "C:\remote\host\test.txt" "C:\local\host\test.txt"
+                                 upload file.exe
+                                 #Need to fix log writing
+                                 #Need to fix errors for non-existing file
+    download                     Download a file from remote host to logging folder. Must specify full path.
+                                 Syntax: download "C:\Users\bob\Desktop\file.pdf"
+                                 #Need to fix log writing
+                                 #Need to fix errors in non-existing file
+    rawcopy                      Copies Binaries\rawcopy.exe to remote host to download locked files using low level disk access. 
+                                 Note: This writes a copy of the target file on the remote system prior to downloading.
+                                 Syntax: rawcopy C:\Windows\System32\config\SAM
+                                 #Need to add file checks before and after transfer
+                                 #Need to fix log writing
+								 #Need to add hash verification
+								 #Need to add support for large files
     users                        Lists subfolders of C:\Users
     exec                         Execute remote powershell command
     netstat                      Execute netstat -an
@@ -109,12 +139,12 @@ Function Help{
                                  Custom syntax: sigcheck C:\Path Entropy
                                  E.g. sigcheck C:\Windows\Temp\B2R 7
     lastactivityview^  lav       Copies Binaries\LastActivityView.exe to remote host and retrieves csv output file
-    rekal*^                      Copy Binaries\rekal.zip to C:\Windows\Temp\B2R on remote host, extract, and run rekal.exe live
+    rekal*^                      Copy Binaries\rekal.zip to remote host, extract, and run rekal.exe live
     cleanup                      Deletes C:\Windows\Temp\B2R on remote host
     exit                quit     Close PSSession and exit B2Response
 
 *Not logged due to technical limitations
-^Requires .\Binaries\PsExec.exe
+^Requires .\Binaries\PsExec.exe 
 
 "@
     Write-Host $help
@@ -143,34 +173,40 @@ While (1 -eq 1) {
         Invoke-Command -Session $s1 -ScriptBlock {Get-Process} | Tee-Object -file "$Logs\pslist.txt" -Append
     }
     If ($Command.StartsWith("upload")){
-        $charCount = ($Command.ToCharArray() | Where-Object {$_ -eq '"'} | Measure-Object).Count
-        if($charCount -eq 4){
-            try{
-                Copy-Item -Path ($Command.split('"')[1]) -Destination ($Command.split('"')[3]) -ToSession $s1
-			    Write-Host "File upload succeeded $Command" | Tee-Object -file "$Logs\upload.txt" -Append
-            }
-            catch{
-                Write-Host "File upload failed: $Command" | Tee-Object -file "$Logs\upload.txt" -Append
-            }
+		TempDir("B2R\uploads")
+        $uploadFile = $Command.substring(7)
+		$uploadFile = $uploadFile.replace('"','')
+		$Path = Get-Location
+		$Path = "$Path\Uploads\$uploadFile"
+        try{
+            Copy-Item -Path "$Path" -Destination "C:\Windows\Temp\B2R\uploads" -ToSession $s1
+        }
+		catch{}
+		if (Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\uploads\$uploadFile}){
+			Write-Host "Successfully uploaded $uploadFile to C:\Windows\Temp\B2R\uploads\$uploadFile" | Tee-Object -file "$Logs\uploads.txt" -Append
 		}
-        else{
-            Write-Host 'Error: must wrap absolute paths in double quotes. E.g. upload "C:\test.txt" "C:\uploads\text.txt"'
-        }
-    }
+		else {
+			Write-Host "Failed upload of $uploadFile to C:\Windows\Temp\B2R\uploads\$uploadFile" | Tee-Object -file "$Logs\uploads.txt" -Append
+		}
+	}
     If ($Command.StartsWith("download")){
-        $charCount = ($Command.ToCharArray() | Where-Object {$_ -eq '"'} | Measure-Object).Count
-        if($charCount -eq 4){
-            try{
-                Copy-Item -Path ($Command.split('"')[1]) -Destination ($Command.split('"')[3]) -FromSession $s1
-			    Write-Host "File download succeeded: $Command" | Tee-Object -file "$Logs\download.txt" -Append
+		LogSubDir("downloads")
+        $downloadFile = $Command.substring(9)
+		$fileName = $downloadFile.split("\")[-1]
+		if ($downloadFile.substring(1) -ne '"' -and $downloadFile.substring($downloadFile.length -1) -ne '"'){
+			$downloadFile = $downloadFile
+		}
+        try{
+            Copy-Item -Path $downloadFile -Destination "$Logs\downloads" -FromSession $s1
+			Write-Host "File download succeeded: $Command" | Tee-Object -file "$Logs\download.txt" -Append
             }
-            catch{
-                Write-Host "File download failed: $Command" | Tee-Object -file "$Logs\download.txt" -Append
-            }
-        }    
-        else{
-            Write-Host 'Error: must wrap absolute paths in double quotes. E.g. download "C:\test.txt" "C:\downloads\text.txt"'
-        }
+		catch{}
+		if (Test-Path "$Logs\downloads\$fileName"){
+			Write-Host "Successfully downloaded $fileName to $Logs\downloads\$fileName" | Tee-Object -file "$Logs\downloads.txt" -Append
+		}
+		else {
+			Write-Host "Failed download of $fileName to $Logs\downloads\$fileName" | Tee-Object -file "$Logs\downloads.txt" -Append
+		}
     }
     If ($Command -eq "users"){
         Invoke-Command -Session $s1 -ScriptBlock{Get-ChildItem -Path C:\Users -Directory -Force -ErrorAction SilentlyContinue | Select-Object Name} | ft -AutoSize | Tee-Object -file "$Logs\users.txt" -Append
@@ -190,6 +226,7 @@ While (1 -eq 1) {
         Invoke-Command -Session $s1 -ScriptBlock {Get-ChildItem C:\Windows\Prefetch -recurse -include @('*.pf')|Select-Object Name, CreationTime, LastWriteTime} | sort LastWriteTime -Descending | ft -autosize | Tee-Object -file "$Logs\prefetch.txt"
     }
     If ($Command -eq "autorunsc"){
+		TempDir("B2R")
         If (-Not(Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\autorunsc.exe})){
             try{
                 Write-Host "Attempting to copy binary to \\$computerName\C$\Windows\Temp\B2R\"
@@ -210,6 +247,7 @@ While (1 -eq 1) {
         }
     }
     If ($Command.StartsWith("sigcheck")){
+		TempDir("B2R")
         $SigEntropy = 7
         $SigPath = "C:\Windows\System32\*"
         If ($Command -ne "sigcheck"){
@@ -242,6 +280,7 @@ While (1 -eq 1) {
         }
     }
     If ($Command -eq "lastactivityview" -or $Command -eq "lav"){
+		TempDir("B2R")
         If (-Not(Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\LastActivityView.exe})){
             try{
                 Write-Host "Attempting to copy binary to \\$computerName\C$\Windows\Temp\B2R\"
@@ -269,6 +308,7 @@ While (1 -eq 1) {
         }
     }
     If ($Command -eq "browsinghistoryview" -or $Command -eq "bhv"){
+		TempDir("B2R")
         If (-Not(Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\BrowsingHistoryView.exe})){
             try{
                 Write-Host "Attempting to copy binary to \\$computerName\C$\Windows\Temp\B2R\"
@@ -295,7 +335,68 @@ While (1 -eq 1) {
             Write-Host "Error retrieving results. Check \\$computerName\C$\Windows\Temp\B2R\browsinghistoryview.csv to see if results were captured on remote host"
         }
     }
+	If ($Command.Startswith("rawcopy")){
+		if ($Command.length -gt 10){
+			$filePath = $Command.substring(8)
+			$filePath = $filePath.replace("/","\")
+			$fileName = $filePath.split("\")[-1]
+			if ((Invoke-Command -Session $s1 -ScriptBlock {param($filePath) Test-Path $filePath} -ArgumentList $filePath) -and $Command.length -gt 10){
+				TempDir("B2R")
+				TempDir("B2R\rawcopy_tmp")
+				LogSubDir("rawcopy_downloads")
+				If (-Not(Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\rawcopy.exe})){
+					try{
+						Write-Host "Attempting to copy binary to \\$computerName\C$\Windows\Temp\B2R\"
+						Copy-Item -Path .\Binaries\rawcopy.exe -Destination C:\Windows\Temp\B2R\rawcopy.exe -ToSession $s1 -ErrorAction Stop
+					}
+					catch{
+						$_.Exception.Message
+					}
+				}
+				If ((Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\rawcopy.exe}) -and ($Command.length -gt 9)) {
+					#exec command
+					Write-Host "Attempting to write temp copy on remote host." 
+					Invoke-Command -Session $s1 -ScriptBlock {param($filePath, $fileName) C:\Windows\Temp\B2R\rawcopy.exe /FileNamePath:$filePath /OutputPath:C:\Windows\Temp\B2R\rawcopy_tmp /OutputName:"$fileName.backup"} -ArgumentList ($filePath, $fileName)
+					If (Invoke-Command -Session $s1 -ScriptBlock {param($fileName) Test-Path C:\Windows\Temp\B2R\rawcopy_tmp\$fileName.backup} -ArgumentList ($fileName)) {
+						Write-Host "Temp copy successfully created. Hashing temp copy before download..."  | Tee-Object -file "$Logs\rawcopy_downloads.txt" -Append
+						$tempHash = (Invoke-Command -Session $s1 -ScriptBlock {param($fileName) Get-FileHash -Path C:\Windows\Temp\B2R\rawcopy_tmp\$fileName.backup -Algorithm MD5} -ArgumentList ($fileName)).Hash
+					}
+					If (Invoke-Command -Session $s1 -ScriptBlock {param($fileName) Test-Path "C:\Windows\Temp\B2R\rawcopy_tmp\$fileName.backup"} -ArgumentList ($fileName)){
+						Write-Host "Attempting to retreive temp file from remote host"
+						Copy-Item -Path "C:\Windows\Temp\B2R\rawcopy_tmp\$fileName.backup" -Destination ".\$Logs\rawcopy_downloads\$fileName.backup" -FromSession $s1
+					}
+					if (Test-Path ".\$Logs\rawcopy_downloads\$fileName.backup"){
+						Write-Host "Remote file $filePath successfully downloaded to rawcopy_downloads\$fileName.backup" | Tee-Object -file "$Logs\rawcopy_downloads.txt" -Append
+						Write-Host "Verifying downloaded copy against temp copy."
+						Write-Host "NOTE: This simply verifies integrity the download (temp copy to downloaded copy). RawCopy does not support hashing of source files."
+						$downloadHash = (Get-FileHash -Path $Logs\rawcopy_downloads\$fileName.backup -Algorithm MD5).Hash
+						if ($tempHash -eq $downloadHash){
+							Write-Host "Temp copy MD5:    $tempHash"
+							Write-Host "Downloaded MD5:   $downloadHash"
+							Write-Host "Verification: Successful Match"
+						}
+						Else {
+							Write-Host "Temp copy MD5:    $tempHash"
+							Write-Host "Downloaded MD5:   $downloadHash"
+							Write-Host "Verification: FAILURE - NO MATCH"
+						}
+					}
+					else {
+						Write-Host "Remote file $filePath failed to download" | Tee-Object -file "$Logs\rawcopy_downloads.txt" -Append
+					}
+				}
+				
+			}
+			else{
+				Write-Host "File does not exist: $filePath"
+			}
+		}
+		else{	
+			Write-Host "Error: Please specify file to copy using syntax: rawcopy C:\Windows\System32\config\SAM" | Tee-Object -file "$Logs\rawcopy_downloads.txt" -Append
+		}
+    }
     If ($Command -eq "rekal"){
+		TempDir("B2R")
         If (-Not(Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\rekal.zip})){
             try{
                 Write-Host "Attempting to copy zip to \\$computerName\C$\Windows\Temp\B2R\"
@@ -314,7 +415,7 @@ While (1 -eq 1) {
 			Write-Host "Missing binary ./Binaries/PsExec.exe"
 		}
         If ((Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\Rekall\rekal.exe}) -and (Test-Path .\Binaries\PsExec.exe)) {
-            .\Binaries\PsExec.exe -s -accepteula \\$computerName -s C:\Windows\Temp\B2R\Rekall\rekal.exe live
+            .\Binaries\PsExec.exe -s -accepteula \\$computerName C:\Windows\Temp\B2R\Rekall\rekal.exe live
         }
     }
     If (($Command -eq "cleanup") -and (Invoke-Command -Session $s1 -ScriptBlock {Test-Path C:\Windows\Temp\B2R\})){
